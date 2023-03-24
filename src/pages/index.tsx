eedcb4bb2c6cc0c1acc0a9bbaceeb6ad409cfba0
Solo Chevez <davidchevez90@gmail.com>
2023-03-23 21:15:55 -0600

    mapping over posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -19,7 +18,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
           {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
-          
+        </div>
+        <div>
+          {data?.map(post => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
